fa5d546adf897c5a07aa915d9a1ceee0a398ecd3
Jia Yi Lai <anniuu7134@gmail.com>
2017-10-12 15:06:33 +0800

    設定回傳預設首頁

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,11 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+//class HomeController extends Controller
+//{
+  //public function index()
+  // {
+    //return'index';
+  //}
+
+//}
 class HomeController extends Controller
 {
-  public function index()
-   {
-    return'index';
-  }
+    public function index()
+    {
 
+return view('welcome');
 }
+}
+
