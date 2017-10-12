6116834b6389a8300f6b5f3581ad8f11d6e4f60d
Jia Yi Lai <anniuu7134@gmail.com>
2017-10-12 14:10:40 +0800

    修改參數成選擇性

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,10 @@
     ///return view('welcome');
    // return redirect('welcome');
 //});
-    Route::get('hello/{name}',function($name){
-        return'Hello,'.$name;
+    //Route::get('hello/{name}',function($name){
+        //return'Hello,'.$name;
+Route::get('hello/{name?}',function($name='Everybody'){
 
-
+return'Hello,'.$name;
+    
 });
